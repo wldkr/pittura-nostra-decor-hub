f5a59b34e7786f815ffe97e6c1d483177644d6f9
wldkr <walid.kerkeni2011@gmail.com>
2025-11-14 11:49:28 +0100

    Update colorsData.ts

--- src/data/colorsData.ts
+++ src/data/colorsData.ts
@@ -1,23 +1,121 @@
 export const colorData = [
-  // A
+  // Ligne 1
   { id: "A01", url: "https://drive.google.com/uc?export=view&id=1nkiPD0gvxU-02Y5RAFac9fbXFiGCB4cS" },
-  { id: "A02", url: "https://drive.google.com/uc?export=view&id=1u2eR8xR9LGx3Y8wSOsSmcIdNBhcSCXVN" },
-  { id: "A03", url: "https://drive.google.com/uc?export=view&id=1HMYtxmHf8vKGQS1GPoqyY57CWbKmuWF4" },
-  // ...
-  { id: "A20", url: "https://drive.google.com/uc?export=view&id=1WcqxpLYiuggPJPv9Xp8BXi7beR0q1rDy" },
-
-  // B
   { id: "B01", url: "https://drive.google.com/uc?export=view&id=1kNi-UvNY8Bl5xDd-9RSfhIgeJ6EI7VWs" },
-  // ...
-  { id: "B20", url: "https://drive.google.com/uc?export=view&id=1SHJEgDm82BObJ_SYDNVmu4A_P9URuiyM" },
-
-  // C
   { id: "C01", url: "https://drive.google.com/uc?export=view&id=17F8BQG3wwiOjr-0Vgttn4MlIsPKUI5ic" },
-  // ...
-  { id: "C20", url: "https://drive.google.com/uc?export=view&id=1iDztqTgXCZn-JtvzFdhcMDYGRkLPBM-3" },
-
-  // D
   { id: "D01", url: "https://drive.google.com/uc?export=view&id=1VeYC_3Gnlx8fHazQOx0tIr6pS0zyEpFb" },
-  // ...
+
+  // Ligne 2
+  { id: "A02", url: "https://drive.google.com/uc?export=view&id=1u2eR8xR9LGx3Y8wSOsSmcIdNBhcSCXVN" },
+  { id: "B02", url: "https://drive.google.com/uc?export=view&id=1YlgYssWHf0oqwcMETyWIeRWmUwipBwS9" },
+  { id: "C02", url: "https://drive.google.com/uc?export=view&id=1UY7VQXr9ziHg4o8TiVx9KXjIg8whX4dZ" },
+  { id: "D02", url: "https://drive.google.com/uc?export=view&id=1dXpUuir754s0QW3kM8vc4Ir1QSvLsfQ9" },
+
+  // Ligne 3
+  { id: "A03", url: "https://drive.google.com/uc?export=view&id=1HMYtxmHf8vKGQS1GPoqyY57CWbKmuWF4" },
+  { id: "B03", url: "https://drive.google.com/uc?export=view&id=1npcSlGvuCwQdy3w5J_a24DAGYH3cblbl" },
+  { id: "C03", url: "https://drive.google.com/uc?export=view&id=1B9JwKXlPRqOtzf4jadvF63mZaZfbVhiA" },
+  { id: "D03", url: "https://drive.google.com/uc?export=view&id=17Aq4OjTxfYzsqfwM2-TXQanU30JpvgTP" },
+
+  // Ligne 4
+  { id: "A04", url: "https://drive.google.com/uc?export=view&id=1-XnSst9TJySfLQx-5uJCWH9TogOAYkHr" },
+  { id: "B04", url: "https://drive.google.com/uc?export=view&id=1nfGPbALcT2bAmX0tRend7kyfzBhMYyHc" },
+  { id: "C04", url: "https://drive.google.com/uc?export=view&id=1Zp5tT6zdhlm8K1GWrBzRea-0Sfg9OLgX" },
+  { id: "D04", url: "https://drive.google.com/uc?export=view&id=17695-ew3YKz-z6PHnGzLPO0yhTe2IQSk" },
+
+  // Ligne 5
+  { id: "A05", url: "https://drive.google.com/uc?export=view&id=1VzG8XSDTVwaF-oG-gwj1OYLT79slW7hO" },
+  { id: "B05", url: "https://drive.google.com/uc?export=view&id=1AznxVfxg0d_c6qjGaIkatuoIlqr45oMJ" },
+  { id: "C05", url: "https://drive.google.com/uc?export=view&id=1Jhgn6M8rAt6nLabmhT06-EImwPAjt9Xv" },
+  { id: "D05", url: "https://drive.google.com/uc?export=view&id=13_Lpx8D0uXWhR9NsMXomQbfTrA58TS37" },
+
+  // Ligne 6
+  { id: "A06", url: "https://drive.google.com/uc?export=view&id=1agu1EZPMnWYCqqTo4cJbGjeqWmuXDbqB" },
+  { id: "B06", url: "https://drive.google.com/uc?export=view&id=15ckOOHHJqMzteXw2q4gB2HqLXB5yCIRN" },
+  { id: "C06", url: "https://drive.google.com/uc?export=view&id=1KuvZKofIKCGTB0f2Ef-nf-jspb35HyzT" },
+  { id: "D06", url: "https://drive.google.com/uc?export=view&id=1McQZE0LR1eQNIGficRXiejNPrRa2ndcK" },
+
+  // Ligne 7
+  { id: "A07", url: "https://drive.google.com/uc?export=view&id=103Yi7SFIPgsGxwUVtjJqBw-m8tG2L-n4" },
+  { id: "B07", url: "https://drive.google.com/uc?export=view&id=13NuJMeMvAcTdH7v5xNGXapqZAH6QMOp7" },
+  { id: "C07", url: "https://drive.google.com/uc?export=view&id=1sreU9KZg2AuvyuTLv2hn5qPlOy6HwqFE" },
+  { id: "D07", url: "https://drive.google.com/uc?export=view&id=1ZRy1jLfxU7swerTsvnGVlSUEnfhhemp3" },
+
+  // Ligne 8
+  { id: "A08", url: "https://drive.google.com/uc?export=view&id=1ohZvBQpqC6nRI5SnRKknhvI9ft9IqolD" },
+  { id: "B08", url: "https://drive.google.com/uc?export=view&id=1k7Tq9jwoZDJAV7Wqp4hCBSXikIcLHMwt" },
+  { id: "C08", url: "https://drive.google.com/uc?export=view&id=1eEJsFeCrDG0EZTClVaX56xi2EmULCQj6" },
+  { id: "D08", url: "https://drive.google.com/uc?export=view&id=1oz_aT-24hhRgBaDmJk_wjDB-x32bDw4k" },
+
+  // Ligne 9
+  { id: "A09", url: "https://drive.google.com/uc?export=view&id=1TRdKn2x-jj-gjEuPucilKHjIKTbL_OVZ" },
+  { id: "B09", url: "https://drive.google.com/uc?export=view&id=1IZo2lHiYbNYQfrKq6UClplhhFU78Ytmy" },
+  { id: "C09", url: "https://drive.google.com/uc?export=view&id=1rDCJvsMg9YyhYm07w4Cr-RANf_pf9fr2" },
+  { id: "D09", url: "https://drive.google.com/uc?export=view&id=1T3bK8TZIAGbRdZ3tO5Z3eJgVx5Q-POcW" },
+
+  // Ligne 10
+  { id: "A10", url: "https://drive.google.com/uc?export=view&id=186DisYUSaQxbuwtxh97A5AYFfQsNY2bW" },
+  { id: "B10", url: "https://drive.google.com/uc?export=view&id=1uK2e-K3FtdmzUM1PbCvswrJ3G5TtbysC" },
+  { id: "C10", url: "https://drive.google.com/uc?export=view&id=15VMHvoqJ2rfI8Lad-hfUOT4AOiyRoYfV" },
+  { id: "D10", url: "https://drive.google.com/uc?export=view&id=1lDvMos0ag0fDyZ1rjHyVJy2t7-aRoctD" },
+
+  // Ligne 11
+  { id: "A11", url: "https://drive.google.com/uc?export=view&id=1QCfqb-8MmerMJDUmRR6FudlLZRzO3cun" },
+  { id: "B11", url: "https://drive.google.com/uc?export=view&id=1AS8PempnVMP49QPtkcaBshPP_iGtiPoX" },
+  { id: "C11", url: "https://drive.google.com/uc?export=view&id=19jNGa5R-nJ-L3dP-Jm_mHzIGBi4ftqaO" },
+  { id: "D11", url: "https://drive.google.com/uc?export=view&id=1629TdvzxQjAVPSMa3tbcrrUqSXoK1nI6" },
+
+  // Ligne 12
+  { id: "A12", url: "https://drive.google.com/uc?export=view&id=1LB7mGsjJw4yCjA-ZKUPl8Iw2KXkUZXd9" },
+  { id: "B12", url: "https://drive.google.com/uc?export=view&id=1x-4owsiQP7MDjye-ZsmMBtAth4-OXluM" },
+  { id: "C12", url: "https://drive.google.com/uc?export=view&id=1KK0kpUBcS9bAjif0iJBp6gs2nZwBHqu6" },
+  { id: "D12", url: "https://drive.google.com/uc?export=view&id=1qHsekD9qHlgGq-OEF8c2pTkjRDCWf_gv" },
+
+  // Ligne 13
+  { id: "A13", url: "https://drive.google.com/uc?export=view&id=1nLXBj3sVZeDLjNaEyBQ9yOK70DBi00MS" },
+  { id: "B13", url: "https://drive.google.com/uc?export=view&id=1mmbxwJzb9QolsrbUZS8TVMuiMASWNPYi" },
+  { id: "C13", url: "https://drive.google.com/uc?export=view&id=1dEL4jXxfskG43c7pcz5n74jUihLobmhO" },
+  { id: "D13", url: "https://drive.google.com/uc?export=view&id=1z9GJPCVmZ6kgcboGHfGdTT_DEMAL4ZDU" },
+
+  // Ligne 14
+  { id: "A14", url: "https://drive.google.com/uc?export=view&id=1y9Q_daUlov11QoKAv691DfaRwGPIbLTl" },
+  { id: "B14", url: "https://drive.google.com/uc?export=view&id=1InmmEJQsYl43SlA1BZEd5lr307NLTl23" },
+  { id: "C14", url: "https://drive.google.com/uc?export=view&id=1wAy4vTUsvNvR3lBp-JpuZxuVF7xLjho9" },
+  { id: "D14", url: "https://drive.google.com/uc?export=view&id=1qS3A-YKGu5g_xz2ndGS6KrPdnWl6kavX" },
+
+  // Ligne 15
+  { id: "A15", url: "https://drive.google.com/uc?export=view&id=1_VRjXgQOHMzKk5TEKEHNBxcUthYOh24p" },
+  { id: "B15", url: "https://drive.google.com/uc?export=view&id=1w6WCriVZqOmLW6YxyoOpFDTzQhE3In6C" },
+  { id: "C15", url: "https://drive.google.com/uc?export=view&id=1Df6h0bdcny67SqfRReyQCzSuZq3lGRW9" },
+  { id: "D15", url: "https://drive.google.com/uc?export=view&id=1YNmTWeqEFPG2yW40lG-jgs_UmCw0C8UI" },
+
+  // Ligne 16
+  { id: "A16", url: "https://drive.google.com/uc?export=view&id=1pmjmfKAOd7olCwBdelVeNzxdWXNzEk-i" },
+  { id: "B16", url: "https://drive.google.com/uc?export=view&id=1Ay4LwzqscxqKET7L7xJBD9BmHFQZ99-F" },
+  { id: "C16", url: "https://drive.google.com/uc?export=view&id=1Ftd6J0u6OrI9bb4xuIysPieDSV6VO3hE" },
+  { id: "D16", url: "https://drive.google.com/uc?export=view&id=1aEIWTKIidHycScMjvh71pjff2sRMXKlv" },
+
+  // Ligne 17
+  { id: "A17", url: "https://drive.google.com/uc?export=view&id=141qkND8PCSazchEXxAxcpnsaGjQjsxNo" },
+  { id: "B17", url: "https://drive.google.com/uc?export=view&id=15DoqQYrKyPGJo_WOVXjLf7qOnVGtmKk-" },
+  { id: "C17", url: "https://drive.google.com/uc?export=view&id=1OSlVWb36gymxnEj2o-bPTNE9Y4odkDLf" },
+  { id: "D17", url: "https://drive.google.com/uc?export=view&id=1uhfhzs0SpweFfBQPoH5CYBdqjb_roVL8" },
+
+  // Ligne 18
+  { id: "A18", url: "https://drive.google.com/uc?export=view&id=1GqAfNBjibbD7ANYKEuQ0TgWIQRzSdxNs" },
+  { id: "B18", url: "https://drive.google.com/uc?export=view&id=1VpO32ipJHnqIxM0e7kxCNkoSfnltqso8" },
+  { id: "C18", url: "https://drive.google.com/uc?export=view&id=1XTNcvN-X7vzVZdunUs4PlHHoys6J4Kww" },
+  { id: "D18", url: "https://drive.google.com/uc?export=view&id=1JoZj1zJsExtTyDAM3ecZ4cQNHWwGB2rW" },
+
+  // Ligne 19
+  { id: "A19", url: "https://drive.google.com/uc?export=view&id=1xKKyPaNr1MItdnrnHNyLkkBciXJgz-As" },
+  { id: "B19", url: "https://drive.google.com/uc?export=view&id=1LaDRqkrcx3KZSJ5SXdPPf2vPmHEcSecd" },
+  { id: "C19", url: "https://drive.google.com/uc?export=view&id=1Wb0YiqdJvv0PgUOKVUZst-kI_KVIb-rn" },
+  { id: "D19", url: "https://drive.google.com/uc?export=view&id=1gmyeMXyfThEulYWOtCyCCTkkkRAk_i3I" },
+
+  // Ligne 20
+  { id: "A20", url: "https://drive.google.com/uc?export=view&id=1WcqxpLYiuggPJPv9Xp8BXi7beR0q1rDy" },
+  { id: "B20", url: "https://drive.google.com/uc?export=view&id=1SHJEgDm82BObJ_SYDNVmu4A_P9URuiyM" },
+  { id: "C20", url: "https://drive.google.com/uc?export=view&id=1iDztqTgXCZn-JtvzFdhcMDYGRkLPBM-3" },
   { id: "D20", url: "https://drive.google.com/uc?export=view&id=1Bpot6_MrtaZwpN7yz-DUh_5lekO-EAOf" },
 ];
