bf369999383743dbda1d3fed9b65849361e7f683
wldkr <walid.kerkeni2011@gmail.com>
2025-11-14 11:10:33 +0100

    Create colorsData.ts

---
+++ src/data/colorsData.ts
@@ -0,0 +1,7 @@
+export const colors = [
+  { id: "A01", url: "https://drive.google.com/uc?export=view&id=1nkiPD0gvxU-02Y5RAFac9fbXFiGCB4cS" },
+  { id: "B01", url: "https://drive.google.com/uc?export=view&id=1kNi-UvNY8Bl5xDd-9RSfhIgeJ6EI7VWs" },
+  { id: "C01", url: "https://drive.google.com/uc?export=view&id=17F8BQG3wwiOjr-0Vgttn4MlIsPKUI5ic" },
+  { id: "D01", url: "https://drive.google.com/uc?export=view&id=1VeYC_3Gnlx8fHazQOx0tIr6pS0zyEpFb" },
+  // … continue pour A02→D20 comme listé précédemment
+];
