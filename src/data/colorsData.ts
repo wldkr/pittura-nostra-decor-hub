cdf568a90d03f65f4d7e0bdb87b576471ac10f4a
wldkr <walid.kerkeni2011@gmail.com>
2025-11-14 11:43:32 +0100

    Update colorsData.ts

--- src/data/colorsData.ts
+++ src/data/colorsData.ts
@@ -1,7 +1,23 @@
-export const colors = [
+export const colorData = [
+  // A
   { id: "A01", url: "https://drive.google.com/uc?export=view&id=1nkiPD0gvxU-02Y5RAFac9fbXFiGCB4cS" },
+  { id: "A02", url: "https://drive.google.com/uc?export=view&id=1u2eR8xR9LGx3Y8wSOsSmcIdNBhcSCXVN" },
+  { id: "A03", url: "https://drive.google.com/uc?export=view&id=1HMYtxmHf8vKGQS1GPoqyY57CWbKmuWF4" },
+  // ...
+  { id: "A20", url: "https://drive.google.com/uc?export=view&id=1WcqxpLYiuggPJPv9Xp8BXi7beR0q1rDy" },
+
+  // B
   { id: "B01", url: "https://drive.google.com/uc?export=view&id=1kNi-UvNY8Bl5xDd-9RSfhIgeJ6EI7VWs" },
+  // ...
+  { id: "B20", url: "https://drive.google.com/uc?export=view&id=1SHJEgDm82BObJ_SYDNVmu4A_P9URuiyM" },
+
+  // C
   { id: "C01", url: "https://drive.google.com/uc?export=view&id=17F8BQG3wwiOjr-0Vgttn4MlIsPKUI5ic" },
+  // ...
+  { id: "C20", url: "https://drive.google.com/uc?export=view&id=1iDztqTgXCZn-JtvzFdhcMDYGRkLPBM-3" },
+
+  // D
   { id: "D01", url: "https://drive.google.com/uc?export=view&id=1VeYC_3Gnlx8fHazQOx0tIr6pS0zyEpFb" },
-  // … continue pour A02→D20 comme listé précédemment
+  // ...
+  { id: "D20", url: "https://drive.google.com/uc?export=view&id=1Bpot6_MrtaZwpN7yz-DUh_5lekO-EAOf" },
 ];
